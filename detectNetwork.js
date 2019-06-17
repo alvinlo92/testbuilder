d4e4a32618559cabd88b9183f882d8f92febdafe
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2019-06-17 16:14:28 -0700

    Completed detectNetwork function

--- detectNetwork.js
+++ detectNetwork.js
@@ -13,6 +13,25 @@ var detectNetwork = function(cardNumber) {
   // The American Express network always starts with a 34 or 37 and is 15 digits long
 
   // Once you've read this, go ahead and try to implement this function, then return to the console.
+  if ((cardNumber.slice(0, 2) === '38' || cardNumber.slice(0, 2) === '39') && cardNumber.length === 14) {
+  	return 'Diner\'s Club';
+  } else if ((cardNumber.slice(0, 2) === '34' || cardNumber.slice(0, 2) === '37') && cardNumber.length === 15) {
+  	return 'American Express';
+  } else if ((cardNumber.slice(0, 4) === '4903' || cardNumber.slice(0, 4) === '4905' || cardNumber.slice(0, 4) === '4911' || cardNumber.slice(0, 4) === '4936' || cardNumber.slice(0, 6) === '564182' || 
+  	cardNumber.slice(0, 6) === '633110' || cardNumber.slice(0, 4) === '6333' || cardNumber.slice(0, 4) === '6759') && (cardNumber.length === 16 || cardNumber.length === 18 || cardNumber.length === 19)) {
+  	return 'Switch';
+  } else if (cardNumber.slice(0, 1) === '4' && (cardNumber.length === 13 || cardNumber.length === 16 || cardNumber.length === 19)) {
+  	return 'Visa';
+  } else if ((cardNumber.slice(0, 2) === '51' || cardNumber.slice(0, 2) === '52' || cardNumber.slice(0, 2) === '53' || cardNumber.slice(0, 2) === '54' || cardNumber.slice(0, 2) === '55') && 
+  	cardNumber.length === 16) {
+  	return 'MasterCard';
+  } else if ((cardNumber.slice(0, 4) === '6011' || cardNumber.slice(0, 3) === '644' || cardNumber.slice(0, 3) === '645' || cardNumber.slice(0, 3) === '646' || cardNumber.slice(0, 3) === '647' 
+  	|| cardNumber.slice(0, 3) === '648' || cardNumber.slice(0, 3) === '649' || cardNumber.slice(0, 2) === '65') && (cardNumber.length === 16 || cardNumber.length === 19)) {
+  	return 'Discover';
+  } else if ((cardNumber.slice(0, 4) === '5018' || cardNumber.slice(0, 4) === '5020' || cardNumber.slice(0, 4) === '5038' || cardNumber.slice(0, 4) === '6304') && (cardNumber.length === 12 || 
+  	cardNumber.length === 13 || cardNumber.length === 14 || cardNumber.length === 15 || cardNumber.length === 16 || cardNumber.length === 17 || cardNumber.length === 18 || cardNumber.length === 19)) {
+  	return 'Maestro';
+  } else if ((cardNumber.slice(0, 6) >= 622126 && cardNumber.slice(0,6) <= 622925) || (cardNumber.slice(0, 3) >= 624 && cardNumber.slice(0, 3) <= 626) || (cardNumber.slice(0,4) >= 6282 && cardNumber.slice(0, 4) <= 6288) && (cardNumber.length >= 16 || cardNumber.length <= 19)) {
+  	return 'China UnionPay';
+  }
 };
-
-
